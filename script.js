60bc57598f85b6afac835ac04d96fe41b24572ec
Willian Carvalho <72278301+Williams26-master@users.noreply.github.com>
2023-04-13 16:18:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let n1 = parseFloat(prompt("Digite um numero"))
+
+let raizsq = Math.sqrt(n1)
+let raizcb = Math.cbrt(n1)
+
+document.write(`A raiz quadrada de ${n1} é igual a: ${raizsq}</br> 
+                e a raiz cubica de ${n1} é igual a: ${raizcb}.`)
